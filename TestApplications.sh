b0f06df081e9a021b6d895f68d7d16717805716d
Chico <63869945+travelingbear@users.noreply.github.com>
2022-05-01 19:15:51 +0100

    Update TestApplications.sh

--- TestApplications.sh
+++ TestApplications.sh
@@ -35,45 +35,6 @@ do
 	echo ""
 	read -p "Press any key to continue ..."
 	clear
-	echo ""
-	echo "Would like this assistant to 'try' to export the app configuration to the image IF the application supports it?"
-	echo "(Y/N)"
-	read as2_allow_config_export
-	echo ""
-	if [[ $as2_allow_config_export == "N" ]] || [[ $as2_allow_config_export == "No" ]] || [[ $as2_allow_config_export == "NO" ]] || [[ $as2_allow_config_export == "n" ]] || [[ $as2_allow_config_export == "no" ]]
-	then
-	  read -p "Press any key to return to the assistant ..."
-	  exit
-	else
-		if [[ $as2_allow_config_export == "Y" ]] || [[ $as2_allow_config_export == "y" ]] || [[ $as2_allow_config_export == "Yes" ]] || [[ $as2_allow_config_export == "YES" ]] || [[ $as2_allow_config_export == "yes" ]];	then
-		  #Creates the skel directory - This directory will be used to save the Default Application Settings for users.
-		  [ ! -d /etc/skel/.config ] && sudo mkdir /etc/skel/.config
-		  
-		  #Here it will try to find the app configuration folder (the one selected from the menu above) and copy it to skel.
-		  if ( ls ~/.config/ | grep -E $var_item_menu ); then
-			app_folder=$(ls ~/.config/ | grep -E $var_item_menu)
-			sudo cp -R ~/.config/app_folder /etc/skel/.config
-			if ( ls /etc/skel/.config | grep -E $var_item_menu ); then
-			  echo ""
-			  echo "App folder copied successfully!"
-			  echo ""
-			  read -p "Press any key to return to the assistant ..."
-			else
-			  echo ""
-			  echo "The assistance couldn't copy the configuration folder $var_item_menu. Does the application supports it?"
-			  echo ""
-			  read -p "Press any key to return to the assistant ..."
-			fi
-		  else 
-			  echo ""
-			  echo "The assistance couldn't find the configuration folder $var_item_menu. Does the application supports it?"
-			  echo ""
-			  read -p "Press any key to return to the assistant ..."
-		  fi
-		else
-			echo "Didn't recognize your input. Did you type Y or N?"
-		fi
-	fi
   fi
 done
 exit
