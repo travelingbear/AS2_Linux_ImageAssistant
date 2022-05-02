a2d0ff05ffd02d91777467e7f12f9bbeb99f276b
Chico <63869945+travelingbear@users.noreply.github.com>
2022-05-02 18:35:19 +0100

    Update TestApplications.sh
    Fixed bugs (hopefully didn't create new ones)
    Added an option to save the configuration file after ADMIN tests

--- TestApplications.sh
+++ TestApplications.sh
@@ -3,23 +3,6 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-
-#Checks if test-user exists. If not, create it.
-does_testuser_exist=$(getent passwd | grep -c '^test-user:')
-if [[ $does_testuser_exist == 0 ]]
-	then 
-	#Creates a test user to test the applications
-	sudo useradd -m test-user 2> /dev/null
-fi
-
-#Checks if test-user is part of the xhost. If not, add it
-is_present_xhost=$(xhost | grep test-user)
-if [[ ! $is_present_xhost ]]
-	then 
-	#Adds the user to the xhost, so it can use X to open desktop apps
-	xhost si:localuser:test-user 2> /dev/null
-fi
-	
 #List the applications and save them in a json file
 sudo AppStreamImageAssistant list-applications > ~/AS2ImageAssistant/as2_app_list.json	
 
@@ -53,12 +36,50 @@ do
   else
 	#If the user didn't cancel, then ask which user to test TEST or ADMIN
     if (whiptail --title "Test Application" --yesno "Would you like to run as ADMIN (to save the settings) or as TEST (to... test)?" 12 78  --no-button "ADMIN" --yes-button "TEST"); then
-		#Runs the command as test-user in the test-user enviroment
+	
+		#Checks if as2-test-user exists. If not, create it.
+		does_testuser_exist=$(getent passwd | grep -c '^as2-test-user:')
+		if [[ $does_testuser_exist == 0 ]]; then    #if it doesn't exist
+			#Creates a test user to test the applications
+			sudo useradd -m as2-test-user 2> /dev/null
+		else
+			#Kills any process owned by as2-test-user; then removes it
+			sudo killall -u as2-test-user 2> /dev/null
+			sudo userdel as2-test-user 2> /dev/null
+			sudo rm -rf /home/as2-test-user 2> /dev/null
+			#And THEN creates it again as a NEW test user to test the applications
+			sudo useradd -m as2-test-user 2> /dev/null
+		fi
+
+		#Checks if as2-test-user is part of the xhost. If not, add it
+		is_present_xhost=$(xhost | grep as2-test-user)
+		if [[ ! $is_present_xhost ]]
+			then 
+			#Adds the user to the xhost, so it can use X to open desktop apps
+			xhost si:localuser:as2-test-user 2> /dev/null
+		fi
+		
+		#Runs the command as as2-test-user in the as2-test-user enviroment
 		clear
 		echo ""
-		read -p "Press enter to run $menu as ${bold}test-user${normal} (you can ignore the warnings)..."
+		read -p "Press enter to run $menu as ${bold}as2-test-user${normal} (you can ignore the warnings)..."
 		echo ""
-		sudo -H -u test-user gnome-terminal -- bash -c $menu
+		sudo -H -u as2-test-user gnome-terminal -- bash -c $menu | grep 'Error constructing proxy for org.gnome.Terminal' &> /dev/null
+		exitmessage=$?
+		
+		#There is an issue with Gnome terminal that can prevent an app from being opened from a different user.
+		#If the EXITMESSAGE got from GREP above is 0, then it tries to kill the user and try again
+		if [[ $exitmessage == 0 ]]; then
+			#Kills any process owned by as2-test-user; then removes it
+			sudo killall -u as2-test-user 2> /dev/null
+			sudo userdel as2-test-user 2> /dev/null
+			sudo rm -rf /home/as2-test-user 2> /dev/null
+			#And then creates it again as a NEW test user to test the applications
+			sudo useradd -m as2-test-user 2> /dev/null
+			#And THEN runs the command again
+			sudo -H -u as2-test-user gnome-terminal -- bash -c $menu | grep 'Error constructing proxy for org.gnome.Terminal' &> /dev/null
+		fi
+		echo ""
 		echo ""
 		clear
 		echo ""
@@ -70,10 +91,12 @@ do
 		echo "Press enter to run $menu as ${bold}ImageBuilderAdmin${normal}"
 		read -p "You can make changes to the app if it supports it"
 		echo ""
-		gnome-terminal -- bash -c $menu 2> /dev/null
-		echo ""
-		echo "------------------------------------"
-		echo ""
+		$menu 2> /dev/null
+		#Ask the user if they want to save the configuration (if supported)
+		if (whiptail --title "Test Application" --yesno "Would you like to save the application settings (if supported)?" 12 78); then
+			sudo rsync -a -v --delete ~/.config/ /etc/skel/.config  2> /dev/null
+			clear
+		fi
 	fi
   fi
 done
