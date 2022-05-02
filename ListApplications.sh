8ac98974386c719988267468e291b3db1d014088
Chico <63869945+travelingbear@users.noreply.github.com>
2022-05-02 23:21:07 +0100

    Update ListApplications.sh
    Added 'Display Name' column

--- ListApplications.sh
+++ ListApplications.sh
@@ -13,9 +13,9 @@ echo "#    These are the current applications in the App Catalog:     #"
 echo "#                                                               #"
 echo "#################################################################"
 echo ""
-echo "APP_NAME" "ABSOLUTE_PATH" > ~/AS2ImageAssistant/app_list.txt
-echo "--------" "-------------" >> ~/AS2ImageAssistant/app_list.txt
-bash -c "paste <(cat ~/AS2ImageAssistant/as2_app_list.json | jq -r '.applications[] .Name') <(cat ~/AS2ImageAssistant/as2_app_list.json | jq -r '.applications[] .AbsoluteAppPath') >> ~/AS2ImageAssistant/app_list.txt"
+echo "APP_NAME" "DISPLAY_NAME" "ABSOLUTE_PATH" > ~/AS2ImageAssistant/app_list.txt
+echo "--------" "------------" "-------------" >> ~/AS2ImageAssistant/app_list.txt
+bash -c "paste <(cat ~/AS2ImageAssistant/as2_app_list.json | jq -r '.applications[] .Name') <(cat ~/AS2ImageAssistant/as2_app_list.json | jq -r '.applications[] .DisplayName') <(cat ~/AS2ImageAssistant/as2_app_list.json | jq -r '.applications[] .AbsoluteAppPath') >> ~/AS2ImageAssistant/app_list.txt"
 cat ~/AS2ImageAssistant/app_list.txt | sed -r 's/\s+/ /g' | column -t -s' '
 echo ""
 read -p "Press any key to return to the assistant ..."
